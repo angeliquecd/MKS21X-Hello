369241a99143a0d32aee3e373fe4d5daead81ff1
angeliquecd <acharles-davis00@stuy.edu>
2018-09-14 21:18:56 -0400

    added code that works

--- Hello.java
+++ Hello.java
@@ -1,1 +1,5 @@
- 
+public class Hello{
+public static void main (String[] args) {
+System.out.println("Hello my name is Angelique, and I don't think that love is really what makes a Subaru a Subaru.");
+}
+} 
